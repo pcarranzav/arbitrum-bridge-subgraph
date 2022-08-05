8f6ad8ac42a07a348d854e4fb6f1eaeab629dfa2
Pablo Carranza Vélez <pablo@edgeandnode.com>
2022-08-05 17:17:02 +0200

    Use the right entity for InboxMessageDelivered

--- src/inbox.ts
+++ src/inbox.ts
@@ -1,32 +1,20 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   Inbox,
-  InboxMessageDelivered,
+  InboxMessageDelivered as InboxMessageDeliveredEvent,
   InboxMessageDeliveredFromOrigin,
   PauseToggled,
   RewriteToggled,
   WhitelistSourceUpdated
 } from "../generated/Inbox/Inbox"
-import { ExampleEntity } from "../generated/schema"
+import { InboxMessageDelivered } from "../generated/schema"
 
 export function handleInboxMessageDelivered(
-  event: InboxMessageDelivered
+  event: InboxMessageDeliveredEvent
 ): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let entity = new InboxMessageDelivered(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
 
   // Entity fields can be set based on event parameters
   entity.messageNum = event.params.messageNum
